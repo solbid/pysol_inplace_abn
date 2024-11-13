54cdab1ebb8e2de63d1700c0342aff43d37ffbdf
Andres Henriquez <154271508+ahenriquezsolbid@users.noreply.github.com>
2024-11-13 10:14:57 -0500

    Update setup.py
    Defaulting to use cuda.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-IABN_FORCE_CUDA = 0#bool(getenv('IABN_FORCE_CUDA', torch.cuda.is_available()))
+IABN_FORCE_CUDA = getenv('IABN_FORCE_CUDA', 1)
 
 if IABN_FORCE_CUDA:
     print("Installing CUDAExtension!!!")
@@ -74,6 +74,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.10",
     ],
     # Versioning
